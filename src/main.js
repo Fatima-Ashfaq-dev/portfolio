26f0a7620f77a11b843d51310a8a5bcd8c1c1406
Fatima Ashfaq <fatimaashfaqupwork@gmail.com>
2025-09-03 20:46:12 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@
 // createApp(App).mount('#app')
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
+import App from '/src/App.vue'
+import '/src/style.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap' 
